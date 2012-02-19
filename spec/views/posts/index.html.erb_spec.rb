7fec1bced8139cbd9d671142e8d35f169b0d91aa
Daniel Luna <dancluna@gmail.com>
2012-02-19 12:26:06 -0200

    Corrected stub model for Post

--- spec/views/posts/index.html.erb_spec.rb
+++ spec/views/posts/index.html.erb_spec.rb
@@ -3,8 +3,8 @@ require 'spec_helper'
 describe "posts/index" do
   before(:each) do
     assign(:posts, [
-      stub_model(Post),
-      stub_model(Post)
+      stub_model(Post, :pub_date => 1.days.ago),
+      stub_model(Post, :pub_date => 2.days.ago)
     ])
   end
 
